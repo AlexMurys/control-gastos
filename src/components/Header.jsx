473a5e61afc5bfb81ec229a1d6c865f9b88dc59f
AlexMurys <skatebcn@gmail.com>
2023-09-12 17:38:19 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Nuevopresupuesto from "./Nuevopresupuesto";
+import ControlPresupuesto from "./ControlPresupuesto";
+
+const Header = ({
+  gastos,
+
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  return (
+    <header>
+      <h1>Planificador de gastos.</h1>
+      {isValidPresupuesto ? (
+        <ControlPresupuesto
+          gastos={gastos}
+          setGastos={setGastos}
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      ) : (
+        <Nuevopresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
+
+export default Header;
