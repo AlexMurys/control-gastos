ae64ca0bb8018d2731cfcdac7e0790b13dab6a97
AlexMurys <skatebcn@gmail.com>
2024-06-25 18:29:16 +0200

    step 1 fordward

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/control-gastos/",
+  base: "/control-gastos/", // Asegúrate de que esto esté configurado correctamente
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
 });
