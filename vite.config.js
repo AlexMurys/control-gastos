6b8592fc25fc8206774363bd09d8495430b1baa7
AlexMurys <skatebcn@gmail.com>
2024-06-25 17:45:59 +0200

    step down 1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/control-gastos/",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "./index.html",
-    },
-  },
 });
