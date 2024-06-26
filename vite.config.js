1a53763565635ffe82e9e55b2dfb62d793bba64c
AlexMurys <skatebcn@gmail.com>
2024-06-26 09:02:43 +0200

    vite.config.js has been modified

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/control-gastos/", // Asegúrate de que esto esté configurado correctamente
+  // base: "/control-gastos/", // Asegúrate de que esto esté configurado correctamente
   build: {
     outDir: "dist",
     rollupOptions: {
